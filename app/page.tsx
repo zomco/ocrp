6e36594bb3f903b00308e6fe1a0b85d4b17a32b9
zomco <zomcoo@gmail.com>
2024-05-30 11:16:23 +0800

    ui bugs fixed

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,29 @@ const SlickArrowNext = ({ className, style, onClick }: CustomArrowProps) => (
 export default function Page() {
     const [current, setCurrent] = useState(0);
     const carouselInput = useRef(null);
+    const [touchStart, setTouchStart] = useState(null)
+    const [touchEnd, setTouchEnd] = useState(null)
+
+    const minSwipeDistance = 50
+    const onTouchStart = (e: any) => {
+        setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+        setTouchStart(e.targetTouches[0].clientY)
+    }
+    const onTouchMove = (e: any) => setTouchEnd(e.targetTouches[0].clientY)
+    const onTouchEnd = () => {
+        if (!touchStart || !touchEnd) return
+        const distance = touchStart - touchEnd
+        const isDownSwipe = distance > minSwipeDistance
+        const isUpSwipe = distance < -minSwipeDistance
+        if (isDownSwipe) {
+            // @ts-ignore
+            carouselInput.current.next();
+        }
+        if (isUpSwipe) {
+            // @ts-ignore
+            carouselInput.current.prev();
+        }
+    }
     return (
         <main className="flex justify-center">
             <div className="w-screen h-dvh portrait:hidden flex justify-center items-center">
@@ -40,7 +63,12 @@ export default function Page() {
                     title="请在竖屏浏览"
                 />
             </div>
-            <div className="landscape:hidden">
+            <div
+                className="landscape:hidden"
+                onTouchStart={onTouchStart}
+                onTouchMove={onTouchMove}
+                onTouchEnd={onTouchEnd}
+            >
                 <Carousel
                     ref={carouselInput}
                     className="w-screen h-dvh min-[430px]:w-[430px] min-[932px]:h-[932px]"
